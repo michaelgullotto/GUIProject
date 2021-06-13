65b567ae77e60b59d0e1378d6610252241e0b943
michaelgullotto <michael2.g@icloud.com>
2021-06-13 16:47:44 +1000

    moevemntsucks need to fix

--- Assets/Scripts/ThirdPersonmovement.cs
+++ Assets/Scripts/ThirdPersonmovement.cs
@@ -12,16 +12,46 @@ public class ThirdPersonmovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothvelocity;
     public int manacost = 20;
-    public Rigidbody rb;
-
+    private Rigidbody rb;
+    public float jumpForce = 15f;
     private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
+    private Vector3 InputVector;
 
-
-    void Update()
+    public void FixedUpdate()
     {
+       
+    }
+    void Update()   
+    {
+        // sprint toggle on and off and sets speed based on movespeed
+        speed = Mystats.Movespeed * sprintspeed / 6;
+        if (sprint == true)
+        {
+            sprintspeed = 2;
+
+        }
+        if (sprint == false)
+        {
+            sprintspeed = 1;
+
+        }
+
+
+
+        // tells player to jump
+        if (Inputmanger.inputmanger.KeyDown("Jump"))
+        { 
+            Vector3 velo = rb.velocity;
+            velo.y = jumpForce;
+            rb.velocity = velo;
+        }
+
+       
+        
+
         // choses which ablity to cast
         if (Inputmanger.inputmanger.KeyDown("Raceablity"))
         {
@@ -54,18 +84,7 @@ public class ThirdPersonmovement : MonoBehaviour
             }
         }
 
-        // sprint toggle on and off
-        speed = Mystats.Movespeed * sprintspeed / 2;
-        if (sprint == true)
-        {
-            sprintspeed = 2;
-            
-        }
-        if (sprint == false)
-        {
-            sprintspeed = 1;
-            
-        }
+     
 
         // stops sprtinging if run out of stamina
         if (Mystats.currentstamina > 0)
@@ -93,11 +112,11 @@ public class ThirdPersonmovement : MonoBehaviour
             StartCoroutine(staminaregen());
         }
 
-      
 
 
 
-        // player direction movement
+
+        //player direction movement
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
@@ -105,17 +124,25 @@ public class ThirdPersonmovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-             
 
+            // turn smoothing
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothvelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+            // movedirection
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            // rb.velocity = moveDir.normalized * speed *Time.deltaTime;
         }
-       
-            
+
+        //// players directional movement
+        //InputVector = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
+        //// rotates player to move direction
+        //transform.LookAt(transform.position + new Vector3(InputVector.x, 0, InputVector.z));
+        //// makes player actully move
+        //rb.velocity = InputVector;
+
+
     }
     // stam usage and regenration
     IEnumerator staminaregen()
